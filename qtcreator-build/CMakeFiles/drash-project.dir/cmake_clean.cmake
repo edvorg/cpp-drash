f1d04dabf2f19d11ce40c5a06b9cf86f22785a62
Edward Knyshov <edvorg@gmail.com>
2012-07-25 02:40:54 +0700

    added project tree

---
+++ qtcreator-build/CMakeFiles/drash-project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/drash-project.dir/main.cpp.o"
+  "drash-project.pdb"
+  "drash-project"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/drash-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
