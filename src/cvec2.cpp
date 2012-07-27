ca039b8b0dc9257b0794e991d87a641cff537981
Edward Knyshov <edvorg@gmail.com>
2012-07-27 17:53:28 +0700

    fixed compilation errors

--- src/cvec2.cpp
+++ src/cvec2.cpp
@@ -4,16 +4,17 @@ namespace drash
 {
 
 CVec2::CVec2():
-    x(0),
-    y(0)
+    b2Vec2( 0.0f, 0.0f )
 {
 }
 
-CVec2::CVec2(float32 _val):b2Vec2(_val,_val)
+CVec2::CVec2(float32 _val):
+    b2Vec2( _val,_val )
 {
 }
 
-CVec2::CVec2(float32 _x, float32 _y):b2Vec2(_x,_y)
+CVec2::CVec2(float32 _x, float32 _y):
+    b2Vec2( _x,_y )
 {
 }
 
