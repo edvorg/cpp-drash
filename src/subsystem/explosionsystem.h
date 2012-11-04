b1158d6600cf872bec9f62f4af011182898f854e
Edward Knyshov <edvorg@gmail.com>
2012-11-04 21:17:10 +0700

    include added

--- src/subsystem/explosionsystem.h
+++ src/subsystem/explosionsystem.h
@@ -1,10 +1,17 @@
 #ifndef EXPLOSIONSYSTEM_H
 #define EXPLOSIONSYSTEM_H
 
+#include <subsystem/subsystem.h>
+
+namespace drash
+{
+
 class CExplosionSystem : public CSubsystem
 {
 public:
     CExplosionSystem();
 };
+
+}// namespace drash
 
 #endif // EXPLOSIONSYSTEM_H
