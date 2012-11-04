4a54c17028f844b209e747631d536988d468f4c9
Edward Knyshov <edvorg@gmail.com>
2012-11-04 23:06:13 +0700

    players system improved

--- src/subsystem/playerssystem.cpp
+++ src/subsystem/playerssystem.cpp
@@ -1,10 +1,66 @@
 #include "playerssystem.h"
 
+#include <diag/clogger.h>
+
 namespace drash
 {
 
-CPlayersSystem::CPlayersSystem()
+CPlayersSystem::CPlayersSystem():
+    mPlayersCount(0)
 {
+}
+
+int CPlayersSystem::AddPlayer(CPlayerParams &_params)
+{
+    if (GetScene() == NULL)
+    {
+        return -1;
+    }
+
+    if (mPlayersCount >= mMaxPlayersCount)
+    {
+        LOG_ERR("CPlayersSystem::AddPlayer(): players count is maximal");
+        return -1;
+    }
+
+    CPlayer *p = GetScene()->CreateObject<CPlayer>(_params);
+
+    if (p != NULL)
+    {
+        mPlayers[mPlayersCount] = p;
+        return mPlayersCount++;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void CPlayersSystem::RemPlayer(int _player)
+{
+    if (GetScene() == NULL)
+    {
+        LOG_ERR("CPlayersSystem::RemPlayer(): scene is unconnected");
+        return;
+    }
+
+    if (_player < 0 && mPlayersCount <= _player)
+    {
+        LOG_ERR("CPlayersSystem::RemPlayer(): player does not exist");
+        return;
+    }
+
+    GetScene()->DestroyObject(mPlayers[_player]);
+}
+
+const CPlayersSystem::CPlayerPtr *CPlayersSystem::GetPlayers()
+{
+    return mPlayers;
+}
+
+unsigned int CPlayersSystem::EnumPlayers() const
+{
+    return mPlayersCount;
 }
 
 }// namespace drash
--- src/subsystem/playerssystem.h
+++ src/subsystem/playerssystem.h
@@ -2,6 +2,7 @@
 #define PLAYERSSYSTEM_H
 
 #include <subsystem/subsystem.h>
+#include <sceneobjects/cplayer.h>
 
 namespace drash
 {
@@ -9,7 +10,20 @@ namespace drash
 class CPlayersSystem : public CSubsystem
 {
 public:
+    typedef CPlayer *CPlayerPtr;
+
     CPlayersSystem();
+
+    int AddPlayer(CPlayerParams &_params);
+    void RemPlayer(int _player);
+    const CPlayerPtr *GetPlayers();
+    unsigned int EnumPlayers() const;
+
+    static const unsigned int mMaxPlayersCount = 10;
+
+private:
+    CPlayerPtr mPlayers[mMaxPlayersCount];
+    unsigned mPlayersCount;
 };
 
 }// namespace drash
