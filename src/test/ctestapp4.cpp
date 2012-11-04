4f18dfeedb74d0c8ef0d96483eb2618cfd618674
Edward Knyshov <edvorg@gmail.com>
2012-11-04 23:14:51 +0700

    test4 extended

--- src/test/ctestapp4.cpp
+++ src/test/ctestapp4.cpp
@@ -1,5 +1,7 @@
 #include "ctestapp4.h"
 
+#include <sceneobjects.h>
+
 namespace drash
 {
 
@@ -13,6 +15,22 @@ bool CTestApp4::Init()
     {
         return false;
     }
+
+    CSceneObjectParams p1;
+    p1.mDynamic = false;
+    p1.mPos.Set(0, -50);
+    p1.mFigures.resize(1);
+    p1.mFigures[0].mLayers.Set(-100,100);
+    p1.mFigures[0].mVertices.push_back( CVec2(-100, -10) );
+    p1.mFigures[0].mVertices.push_back( CVec2(100, -10) );
+    p1.mFigures[0].mVertices.push_back( CVec2(100, 10) );
+    p1.mFigures[0].mVertices.push_back( CVec2(-100, 10) );
+    GetScene().CreateObject<CSceneObject>(p1);
+
+    CPlayerParams p2;
+    p2.mFigures.resize(1);
+    p2.mFigures[0].mLayers.Set(-100,100);
+    GetPlayersSystem().AddPlayer(p2);
 
     return true;
 }
