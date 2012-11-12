940661336f79959297da4c440096b9effd8be1dc
Edward Knyshov <edvorg@gmail.com>
2012-11-12 23:16:40 +0700

    CTemplateSystem improved

--- src/subsystem/templatesystem.cpp
+++ src/subsystem/templatesystem.cpp
@@ -1,5 +1,37 @@
 #include "templatesystem.h"
 
+namespace drash
+{
+
 CTemplateSystem::CTemplateSystem()
 {
 }
+
+CSceneObjectTemplate *CTemplateSystem::CreateSceneObjectTemplate(const std::string _name)
+{
+    if (_name == "")
+    {
+        return nullptr;
+    }
+    else
+    {
+        mSceneObjectGeometries.push_back(CSceneObjectTemplate());
+        mSceneObjectGeometries.back().mName = _name;
+        return &mSceneObjectGeometries.back();
+    }
+}
+
+CSceneObject *CTemplateSystem::CreateSceneObjectFromTemplate(const std::string &_name, const CSceneObjectParams &_params)
+{
+    for (auto i=mSceneObjectGeometries.begin(), i_e=mSceneObjectGeometries.end(); i!=i_e; i++)
+    {
+        if (i->mName == _name)
+        {
+            return GetScene()->CreateObject<CSceneObject>(i->mGeometry, _params);
+        }
+    }
+
+    return nullptr;
+}
+
+}// namespace drash
--- src/subsystem/templatesystem.h
+++ src/subsystem/templatesystem.h
@@ -2,11 +2,35 @@
 #define TEMPLATESYSTEM_H
 
 #include "subsystem.h"
+#include "../sceneobjects.h"
+#include <vector>
+
+namespace drash
+{
+
+class CSceneObjectTemplate
+{
+public:
+    CSceneObjectGeometry mGeometry;
+    std::string mName;
+protected:
+private:
+};
 
 class CTemplateSystem : public CSubsystem
 {
 public:
+    typedef std::vector<CSceneObjectTemplate> SceneObjectTemplatesT;
+
     CTemplateSystem();
+
+    CSceneObjectTemplate* CreateSceneObjectTemplate(const std::string _name);
+    CSceneObject *CreateSceneObjectFromTemplate(const std::string &_name, const CSceneObjectParams &_params);
+protected:
+private:
+    SceneObjectTemplatesT mSceneObjectGeometries;
 };
+
+}// namespace drash
 
 #endif // TEMPLATESYSTEM_H
