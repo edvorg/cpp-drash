cc187c327232dd2f492cd0752ed69495f85f0a00
Edward Knyshov <edvorg@gmail.com>
2012-11-24 13:35:51 +0700

    added fov (in radians) to camera

--- src/sceneobjects/ccamera.h
+++ src/sceneobjects/ccamera.h
@@ -21,13 +21,27 @@ public:
 
     typedef CCameraParams ParamsT;
 
+    inline void SetFov(float _fov);
+    inline float GetFov() const;
+
 protected:        
     CCamera( void );
 
     bool Init( const GeometryT &_geometry, const ParamsT &_params );
 
 private:
+    float mFov = M_PI / 3.0f;
 };
+
+inline void CCamera::SetFov(float _fov)
+{
+    mFov = _fov;
+}
+
+inline float CCamera::GetFov() const
+{
+    return mFov;
+}
 
 }// namespace drash
 
