fc1379779fbdc65ec543ca56a5272ec64f871b7e
Edward Knyshov <edvorg@gmail.com>
2012-12-20 16:42:40 +0700

    CRay improved

--- src/misc/ray.cpp
+++ src/misc/ray.cpp
@@ -7,4 +7,15 @@ CRay::CRay()
 {
 }
 
+void CRay::SetPoint(const CVec3f &_point)
+{
+    mPoint = _point;
+}
+
+void CRay::SetDirection(const CVec3f &_direction)
+{
+    mDirection = _direction;
+    mDirection.Normalize();
+}
+
 } // namespace drash
--- src/misc/ray.h
+++ src/misc/ray.h
@@ -1,6 +1,8 @@
 #ifndef DRASH_RAY_H
 #define DRASH_RAY_H
 
+#include "vec3.h"
+
 namespace drash
 {
 
@@ -8,7 +10,26 @@ class CRay
 {
 public:
     CRay();
+
+    void SetPoint(const CVec3f &_point);
+    inline const CVec3f &GetPoint() const;
+    void SetDirection(const CVec3f &_direction);
+    inline const CVec3f &GetDirection() const;
+
+private:
+    CVec3f mPoint;
+    CVec3f mDirection;
 };
+
+inline const CVec3f &CRay::GetPoint() const
+{
+    return mPoint;
+}
+
+inline const CVec3f &CRay::GetDirection() const
+{
+    return mDirection;
+}
 
 } // namespace drash
 
