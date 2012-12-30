42197e3b2acd3f9cd33f2f19e454336dd658487c
Edward Knyshov <edvorg@gmail.com>
2012-12-30 14:32:02 +0700

    CColor4 generic improved

--- src/misc/color4.h
+++ src/misc/color4.h
@@ -11,10 +11,13 @@ class CColor4 : public CColor3<T, DEF_VAL_FUNC>
 {
 public:
     CColor4() = default;
-    CColor4(const CColor4 &_rgb);
-    CColor4(const T &_rgb);
+    CColor4(const CColor4 &_rgba);
+    CColor4(const CColor3<T, DEF_VAL_FUNC> &_rgb, const T &_a);
+    CColor4(const T &_rgba);
+    CColor4(const T &_r, const T &_g, const T &_b);
     CColor4(const T &_r, const T &_g, const T &_b, const T &_a);
 
+    CColor4 &Set(const CColor3<T, DEF_VAL_FUNC> &_rgb, const T &_a);
     CColor4 &Set(const T &_r, const T &_g, const T &_b, const T &_a);
 
     /// conversion
@@ -35,16 +38,29 @@ typedef CColor4<unsigned char, CColorDefValUnsignedByte> CColor4ub;
 /// CColor3 implementation
 
 template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
-CColor4<T, DEF_VAL_FUNC>::CColor4(const CColor4 &_rgb):
-    CColor3<T, DEF_VAL_FUNC>(_rgb),
-    mA(_rgb.mA)
+CColor4<T, DEF_VAL_FUNC>::CColor4(const CColor4 &_rgba):
+    CColor3<T, DEF_VAL_FUNC>(_rgba),
+    mA(_rgba.mA)
 {
 }
 
 template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
-CColor4<T, DEF_VAL_FUNC>::CColor4(const T &_rgb):
+CColor4<T, DEF_VAL_FUNC>::CColor4(const CColor3<T, DEF_VAL_FUNC> &_rgb, const T &_a):
     CColor3<T, DEF_VAL_FUNC>(_rgb),
-    mA(_rgb)
+    mA(_a)
+{
+}
+
+template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
+CColor4<T, DEF_VAL_FUNC>::CColor4(const T &_rgba):
+    CColor3<T, DEF_VAL_FUNC>(_rgba),
+    mA(_rgba)
+{
+}
+
+template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
+CColor4<T, DEF_VAL_FUNC>::CColor4(const T &_r, const T &_g, const T &_b):
+    CColor3<T, DEF_VAL_FUNC>(_r, _g, _b)
 {
 }
 
@@ -53,6 +69,16 @@ CColor4<T, DEF_VAL_FUNC>::CColor4(const T &_r, const T &_g, const T &_b, const T
     CColor3<T, DEF_VAL_FUNC>(_r, _g, _b),
     mA(_a)
 {
+}
+
+template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
+CColor4<T, DEF_VAL_FUNC> &CColor4<T, DEF_VAL_FUNC>::Set(const CColor3<T, DEF_VAL_FUNC> &_rgb, const T &_a)
+{
+    this->mR = _rgb.mR;
+    this->mG = _rgb.mG;
+    this->mB = _rgb.mB;
+    this->mA = _a;
+    return *this;
 }
 
 template<class T, T (*DEF_VAL_FUNC) (unsigned int _comp_index)>
