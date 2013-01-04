54ba0bc8c170639dd277e86ddb6d42eb1f90fc36
Edward Knyshov <edvorg@gmail.com>
2013-01-05 04:12:58 +0700

    Using linear filters for png textures

--- src/greng/texturemanager.cpp
+++ src/greng/texturemanager.cpp
@@ -102,8 +102,8 @@ CTexture *CTextureManager::CreateTextureFromFile(const char *_path)
                                (s->format->Amask ? 1 : 0);
 
     glBindTexture(GL_TEXTURE_2D, res->mTextureBufferId);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D,
                  0,
                  comps_count,
