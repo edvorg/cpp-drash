ec50dc83784a7fb4d6e21a230f541231293f4b95
Yura Shatilin <yra.mind@gmail.com>
2013-01-12 06:57:26 +0700

    new Methods Load and Store in Level

--- src/levelmanager/level.cpp
+++ src/levelmanager/level.cpp
@@ -24,6 +24,8 @@ along with drash Source Code.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "level.h"
 
+using namespace std;
+
 namespace drash
 {
 
@@ -34,7 +36,9 @@ CSceneObjectParams *CLevel::AddObject(const std::string &_template,
 
     if (template_objects == mObjects.end())
     {
-        template_objects = mObjects.insert(pair<string, CLevel::LEVEL_NODE>(_template, CLevel::LEVEL_NODE())).first;
+        template_objects = mObjects.insert(pair<string,
+                                           CLevel::LEVEL_NODE>(_template,
+                                                               CLevel::LEVEL_NODE())).first;
     }
 
     auto object = template_objects->second.find(_name);
@@ -45,7 +49,78 @@ CSceneObjectParams *CLevel::AddObject(const std::string &_template,
         return nullptr;
     }
 
-    return &template_objects->second.insert(pair<string, CSceneObjectParams>(_name, CSceneObjectParams())).first->second;
+    return &template_objects->second.insert(pair<string,
+                                            CSceneObjectParams>(_name,
+                                                                CSceneObjectParams())).first->second;
+}
+
+bool CLevel::Store(const string &_filename) const
+{
+    ofstream out(_filename);
+
+    if (out.is_open() == false) {
+        LOG_ERR("Can not load level from file " << _filename.c_str());
+        return false;
+    }
+
+//    map<string, LEVEL_NODE> mObjects;
+    out << mObjects.size() << endl;
+    for (auto &itemTemplates : mObjects) {
+        // Save name Template
+        out << itemTemplates.first << endl;
+        out << itemTemplates.second.size() << endl;
+        for (auto &ObjectItem : itemTemplates.second) {
+            out << ObjectItem.first << endl;
+            const CSceneObjectParams &cur = ObjectItem.second;
+            out << cur.mAngle << endl;
+            out << cur.mDynamic << endl;
+            out << cur.mFixedRotation << endl;
+            out << cur.mPos.mX << endl;
+            out << cur.mPos.mY << endl;
+            out << cur.mPos.mZ << endl;
+        }
+    }
+
+    out.close();
+}
+
+bool CLevel::Load(const string &_filename)
+{
+    ifstream in(_filename);
+
+    if (in.is_open() == false) {
+        LOG_ERR("Can not store level from file " << _filename.c_str());
+        return false;
+    }
+
+    mObjects.clear();
+
+    int count_templates = 0;
+    in >> count_templates;
+    for (int i = 0 ; i < count_templates ; i++) {
+        std::string name_template = "";
+        in >> name_template;
+        int count_object = 0;
+        in >> count_object;
+        LEVEL_NODE buffMap;
+        for (int i = 0 ; i < count_object ; i++) {
+            string name_object = "";
+            CSceneObjectParams params;
+            in >> name_object;
+            in >> params.mAngle;
+            in >> params.mDynamic;
+            in >> params.mFixedRotation;
+            in >> params.mPos.mX;
+            in >> params.mPos.mY;
+            in >> params.mPos.mZ;
+            pair<string,CSceneObjectParams> p(name_object,params);
+            buffMap.insert(p);
+        }
+        pair<string,LEVEL_NODE> p(name_template,buffMap);
+        mObjects.insert(p);
+    }
+
+    in.close();
 }
 
 } // namespace drash
