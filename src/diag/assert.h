9452f7e0faf9afa3114fdffbbc63721701d13c57
Edward Knyshov <edvorg@gmail.com>
2013-10-10 23:01:04 +0700

    licenses

--- src/diag/assert.h
+++ src/diag/assert.h
@@ -1,23 +1,23 @@
 // DRASH_LICENSE_BEGIN
 /*
 
-drash GPL Source Code
-Copyright (C) 2012-2013 Edward Knyshov, Yuriy Shatilin.
+  drash GPL Source Code
+  Copyright (C) 2012-2013 Edward Knyshov, Yuriy Shatilin.
 
-This file is part of the drash GPL Source Code (drash Source Code).
+  This file is part of the drash GPL Source Code (drash Source Code).
 
-drash Source Code is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  drash Source Code is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-drash Source Code is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  drash Source Code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with drash Source Code.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with drash Source Code. If not, see <http://www.gnu.org/licenses/>.
 
 */
 // DRASH_LICENSE_END
